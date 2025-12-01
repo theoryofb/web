e6489692db0ea2f2076cd74b3d9a08e65ee58669
gians <gian.villca@sistemas.edu.bo>
2025-12-01 15:52:33 -0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -4,20 +4,24 @@ def inicializar_bd():
     conn = sqlite3.connect("eventos.db")
     cur = conn.cursor()
 
-    # Crear tabla si no existe
     cur.execute("""
         CREATE TABLE IF NOT EXISTS eventos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tipo TEXT NOT NULL,
             nombre TEXT NOT NULL,
-            descripcion TEXT,
+            carnet TEXT NOT NULL,
+            direccion_domicilio TEXT NOT NULL,
+            monto_garantia REAL NOT NULL,
+            monto_total REAL NOT NULL,
             dia TEXT NOT NULL,
-            hora TEXT NOT NULL
+            hora_fin TEXT NOT NULL,
+            decoracion INTEGER NOT NULL
         );
     """)
 
     conn.commit()
     conn.close()
-    print("Base de datos inicializada correctamente.")
+    print("Base de datos lista.")
 
 if __name__ == "__main__":
     inicializar_bd()
